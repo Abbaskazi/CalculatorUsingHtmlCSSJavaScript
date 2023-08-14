45acbd182fb38c8323dd9d72ca9e34961a35d4e0
Abbaskazi <139066960+Abbaskazi@users.noreply.github.com>
2023-08-14 14:26:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var string = ""
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click' , (e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'Cancel'){
+            string = string.slice(0,-1); 
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'C'){
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else{
+            console.log(e.target);
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+})
